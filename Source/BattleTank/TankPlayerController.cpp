a231b2425ed52762e9e8d905f3b702bdb5f6c1cb
Sergey Miroshnichenko <ommmirage@gmail.com>
2019-12-10 12:55:28 +0400

    ATankPlayerController::GetSightRayHitLocation()

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,8 @@
 #include "BattleTank.h"
 #include "TankPlayerController.h"
 
+#define OUT
+
 void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
@@ -30,5 +32,55 @@ void ATankPlayerController::AimTowardsCrosshair()
 {
 	if (!GetControlledTank()) { return; }
 
-	// Get world location if linetrace
+	FVector OutHitLocation;
+
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *OutHitLocation.ToString());
+	// Get world location of linetrace through crosshair
+	if (GetSightRayHitLocation(OutHitLocation))
+	{
+		// Aim there
+		UE_LOG(LogTemp, Warning, TEXT("%s"), *OutHitLocation.ToString());
+	}
+	
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector &OutHitLocation)
+{
+	FCollisionQueryParams CollisionParams;
+	FHitResult HitResult;
+
+	GetWorld()->LineTraceSingleByChannel(
+		OUT HitResult,
+		GetSightRayStart(),
+		GetSightRayEnd(),
+		ECollisionChannel::ECC_PhysicsBody,
+		CollisionParams
+	);
+	
+	OutHitLocation=HitResult.Location;
+
+	return (!OutHitLocation.IsNearlyZero());
+}
+
+FVector ATankPlayerController::GetSightRayStart()
+{
+	TArray<UStaticMeshComponent*> StaticMeshComponents;
+
+	GetControlledTank()->GetComponents<UStaticMeshComponent>(OUT StaticMeshComponents);
+
+	UStaticMeshComponent* Barrel = StaticMeshComponents[2];
+	FVector BarrelEndRalativeLocation = Barrel->GetSocketByName("BarrelEnd")->RelativeLocation;
+	return (Barrel->GetComponentLocation()+ BarrelEndRalativeLocation);
+}
+
+FVector ATankPlayerController::GetSightRayEnd()
+{
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	return PlayerViewPointLocation + PlayerViewPointRotation.Vector() * ShotingDistance;
 }
--- Source/BattleTank/TankPlayerController.h
+++ Source/BattleTank/TankPlayerController.h
@@ -19,10 +19,18 @@ public:
 	virtual void BeginPlay() override;
 
 private:
+	float ShotingDistance = 50000.f;
 	ATank* GetControlledTank() const;
 
 	virtual void Tick(float DeltaTime) override;
 
 	// Moves a barrel to a crosshair (прицел)
 	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation);
+
+	FVector GetSightRayStart();
+	
+	FVector GetSightRayEnd();
 };
